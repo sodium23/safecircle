4ec90cec815f87a79a47bf83766683633d346efc
Niyati <niyatiagarwal2901@gmail.com>
2026-03-26 15:50:20 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,11 +33,17 @@ def _build_allowed_origins() -> List[str]:
 
 app.add_middleware(
     CORSMiddleware,
-     allow_origins=["*"],
+  allow_origins=[
+        "https://safecircle1.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 # ---------------------------
